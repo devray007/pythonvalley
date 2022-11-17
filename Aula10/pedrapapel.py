7d8030e8591075a9300f2618e73829e5aee9b635
devray007 <raiwhaiby10@gmail.com>
2022-11-17 19:31:38 -0300

    brincadeirinha

---
+++ Aula10/pedrapapel.py
@@ -0,0 +1,38 @@
+print("Bem vindo ao jogo de pedra, papel e tesoura")
+
+itens = ("pedra","papel,","tesoura")
+
+maquina = 
+
+
+
+
+
+
+
+if maquina == 0:_# maquina jogou preda
+if jogador == 0:
+    print("EMPATE,BOA!")
+
+    elif jogador == 1
+    print("VENCEU MISERAVEL!")
+
+    elif jogador == 2
+    print("MAQUINA VENCEU HAHAHA!")
+
+else:
+    print("NUMERO INVALIDO")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+print("FIM DO JOGO")
